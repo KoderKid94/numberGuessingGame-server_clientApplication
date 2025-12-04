80b2f3792126e51b8cf12d0b9d46971363e5a1e1
KoderKid94 <12345678+KoderKid94@users.noreply.github.com>
2025-12-04 05:21:18 -0600

    updates to server.py as well as started implementing client.py

--- server/room.py
+++ server/room.py
@@ -1,24 +1,42 @@
 import random
+from enum import Enum
+
+class GuessResult(Enum):
+    CORRECT = 0
+    TOO_LOW = 1
+    TOO_HIGH = 2
+    GAME_OVER = 3
 
 class Room:
     def __init__(self, client1, client2):
         self.correct_number = self.generate_number()
         self.game_over = False
 
-    # generate random integer between given values
-    def generate_number(self):
-        return random.randomint(1, 500)
+    # generate random integer between given values, helper method so we make it static
+    @staticmethod
+    def generate_number():
+        return random.randint(1, 500)
 
-    def verify_guess(self, client, guess):
+    def verify_guess(self, guess):
         # if the game is over, there should be no guess submissions
         if self.game_over:
-            return False
+            return GuessResult.GAME_OVER
 
-        # verify if the clients guess is correct or not
-        correct = (self.correct_number == guess)
-
-        if correct:
+        # verify if the clients guess is correct, too low/high.
+        if self.correct_number == guess:
             self.game_over = True
+            return GuessResult.CORRECT
 
-        return correct
+        if self.correct_number > guess:
+            return GuessResult.TOO_LOW
+
+        return GuessResult.TOO_HIGH
+
+
+
+
+
+
+
+
 
